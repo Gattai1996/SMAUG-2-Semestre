c39af27a22ddc1a6a9dd5553d1ac5b612d2cb17e
Gattai1996 <46097759+Gattai1996@users.noreply.github.com>
2019-05-14 15:07:13 -0300

    beta_1.3
    Efeito parallax, level design, animações e itens novos

---
+++ Mummy/maps/background_descending@2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="background_descending@2x" tilewidth="64" tileheight="64" tilecount="216" columns="9">
+ <image source="../../../Desktop/Assets/Sprites/GameArtPartners_Freebies/GameArtPartners_Freebies/WorldWarMech_Lava_Station/PNG_Exports_1x_2x_3x/background_descending@2x.png" width="602" height="1536"/>
+</tileset>
