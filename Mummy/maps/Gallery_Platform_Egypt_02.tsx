4d1b0971a266db035dce59f28fa61fe5ffad7992
Gattai1996 <46097759+Gattai1996@users.noreply.github.com>
2019-05-17 01:43:14 -0300

    beta_1.3
    Inimigos com IA

---
+++ Mummy/maps/Gallery_Platform_Egypt_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Gallery_Platform_Egypt_02" tilewidth="64" tileheight="64" tilecount="12" columns="3">
+ <image source="../../../../../../SMAUG/Gallery_Platform_Egypt_02.png" width="192" height="256"/>
+</tileset>
---
+++ Mummy/maps/spike2@2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="spike2@2x" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="spike2@2x.png" width="193" height="85"/>
+</tileset>
