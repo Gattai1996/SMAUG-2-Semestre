6b90a1d105cdf83f164c2b6648733b6c7e16487f
Gattai1996 <46097759+Gattai1996@users.noreply.github.com>
2019-05-18 19:15:36 -0300

    beta_1.3
    Erro nos arquivos .tmx concertados
    Adicionado: detalhes de level design e placas para ajudar  jogador

---
+++ Mummy/maps/spike grande.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="spike grande" tilewidth="193" tileheight="85" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="193" height="85" source="spike2@2x.png"/>
+ </tile>
+</tileset>
---
+++ Mummy/maps/spike2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="spike2" tilewidth="97" tileheight="43" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="97" height="43" source="spike2.png"/>
+ </tile>
+</tileset>
